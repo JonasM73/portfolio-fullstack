c7b2c6eec54d88dcd50e600a545bf5b707913899
Jonas-Cesi <jona.mionnet@viacesi.fr>
2026-06-12 16:58:21 +0200

    secu 4.4

--- Portfolio.Gateway/Program.cs
+++ Portfolio.Gateway/Program.cs
@@ -52,6 +52,7 @@ app.Use(async (context, next) =>
 
         context.Response.Headers["X-Content-Type-Options"] = "nosniff";
         context.Response.Headers["X-Frame-Options"] = "DENY";
+        context.Response.Headers["Referrer-Policy"] = "strict-origin-when-cross-origin";
 
     await next();
 });
